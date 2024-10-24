19f3ee6f6a7d5a41c842a91e4a237cf244b9b8c8
Chris <chrishughallen@gmail.com>
2024-10-24 14:49:03 +0200

    add sizes param to title and update previews

--- app/components/title_component.rb
+++ app/components/title_component.rb
@@ -1,10 +1,32 @@
 class TitleComponent < ViewComponent::Base
-  def initialize(semantic_tag: "div")
+  def initialize(semantic_tag: "div", size: nil)
     @semantic_tag = semantic_tag
+    @size = size
   end
 
   def call
-    content_tag @semantic_tag, content, class: "title"
+    content_tag @semantic_tag, content, class: css_class
+  end
+
+  def css_class
+    "title #{add_css_class_for_size}"
+  end
+
+  def add_css_class_for_size
+    return unless @size.present?
+
+    case @size
+    when "sm"
+      "title--sm"
+    when "md"
+      "title--md"
+    when "lg"
+      "title--lg"
+    when "xl"
+      "title--xl"
+    when "xxl"
+      "title--xxl"
+    end
   end
 
   attr_reader :semantic_tag
--- test/components/previews/title_component_preview.rb
+++ test/components/previews/title_component_preview.rb
@@ -1,9 +1,74 @@
 class TitleComponentPreview < ViewComponent::Preview
+  # Default title
+  # ---------------
+  # This is the title component with the default settings.
+  #
+  # @label Default
   def default
     render(TitleComponent.new.with_content("Hello, world!"))
   end
 
-  def with_h1_semantic_tag
+  # @!group Semantic tags
+  # Semantic Tags
+  # ---------------
+  # Change the semantic tag of the title component by passing the `semantic_tag` param.
+  #
+  # Don't rely on the semantic tag to change the display of the title as it only relates to html semantics.
+  def h1
     render(TitleComponent.new(semantic_tag: "h1").with_content("Hello, world!"))
   end
+
+  def h2
+    render(TitleComponent.new(semantic_tag: "h2").with_content("Hello, world!"))
+  end
+
+  def h3
+    render(TitleComponent.new(semantic_tag: "h3").with_content("Hello, world!"))
+  end
+
+  def h4
+    render(TitleComponent.new(semantic_tag: "h4").with_content("Hello, world!"))
+  end
+
+  def h5
+    render(TitleComponent.new(semantic_tag: "h5").with_content("Hello, world!"))
+  end
+
+  def h6
+    render(TitleComponent.new(semantic_tag: "h6").with_content("Hello, world!"))
+  end
+
+  # @!endgroup
+  #
+  #
+  # @!group Sizes
+  # Sizes
+  # ---------------
+  # Change the size of the title component by passing the `size` param.
+  #
+  # @label S
+  def small
+    render(TitleComponent.new(size: "sm").with_content("Hello, world!"))
+  end
+
+  # @label M
+  def medium
+    render(TitleComponent.new(size: "md").with_content("Hello, world!"))
+  end
+
+  # @label L
+  def large
+    render(TitleComponent.new(size: "lg").with_content("Hello, world!"))
+  end
+
+  # @label XL
+  def extra_large
+    render(TitleComponent.new(size: "xl").with_content("Hello, world!"))
+  end
+
+  # @label XXL
+  def extra_extra_large
+    render(TitleComponent.new(size: "xxl").with_content("Hello, world!"))
+  end
+  # @!endgroup
 end
